d40de2f202c06e0fdacbd2fad03b6e2795cde7de
Deenash <deenash.a.s@gmail.com>
2017-11-02 23:51:26 -0700

    Added Error Handling; Avoided the use of Array.reduce

--- jokes.js
+++ jokes.js
@@ -1,12 +1,16 @@
 var fetch = require('node-fetch');
 
-function reduceString(result, temp){
-	return result + '\n- ' + temp;
-}
+//function reduceString(result, temp){
+//	return result + '\n- ' + temp;
+//}
 
 fetch('http://api.icndb.com/jokes/random/5?limitTo=[nerdy]')
-.then(function(res) {
+.then(
+  function(res) {
     return res.json();
+	},
+  function(err) {
+  	console.log('error');  
 }).then(function(body){
 	var values = body.value;
 	var jokes = [];
@@ -15,8 +19,12 @@ fetch('http://api.icndb.com/jokes/random/5?limitTo=[nerdy]')
 			jokes.push(values[value].joke);
 		}
 	}
-	
 	if(jokes && jokes.length > 0) {
-		console.log('- ' + jokes.reduce(reduceString));
+		// we can also use the Array.reduce here
+		// console.log('- ' + jokes.reduce(reduceString));
+	    for(joke in jokes){
+	    	console.log('- ' + jokes[joke]);
+	    }
 	}
 });
+
